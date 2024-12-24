27fc8293ead3b39c23ccc2cb470a35ac88b3ceb9
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2024-12-24 23:47:56 +0530

    Time: 1 ms (87.68%) | Memory: 61 MB (36.30%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,19 +4,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-var rotate = function(nums, k) {
-    k = k % nums.length;  // To handle cases where k > nums.length
-    const temp=[];
-    for(let i=nums.length-1;i>=nums.length-k;i--){
-        temp.push(nums[i]);
-    }
+// var rotate = function(nums, k) {
+//     k = k % nums.length;  // To handle cases where k > nums.length
+//     const temp=[];
+//     for(let i=nums.length-1;i>=nums.length-k;i--){
+//         temp.push(nums[i]);
+//     }
 
-    for(let j=nums.length-1;j>=k;j--){
-        nums[j]=nums[j-k];
-    }
-    temp.reverse()
-    for(let z=0;z<k;z++){
-        nums[z]=temp[z]
-    }
+//     for(let j=nums.length-1;j>=k;j--){
+//         nums[j]=nums[j-k];
+//     }
+//     temp.reverse()
+//     for(let z=0;z<k;z++){
+//         nums[z]=temp[z]
+//     }
+
+// };
+
+//more readable
+
+var rotate = function(nums, k) {
+    k=k% nums.length
+    nums.unshift(...   nums.splice(nums.length-k))
 
 };
