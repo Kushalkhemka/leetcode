410cc3f59df527ec7c86e9546bfdf7b66d2da5c2
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2024-12-24 23:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 59.7 MB (71.63%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,24 +4,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-// var rotate = function(nums, k) {
-//     k = k % nums.length;  // To handle cases where k > nums.length
-//     const temp=[];
-//     for(let i=nums.length-1;i>=nums.length-k;i--){
-//         temp.push(nums[i]);
-//     }
+ //less space complexity
 
-//     for(let j=nums.length-1;j>=k;j--){
-//         nums[j]=nums[j-k];
-//     }
-//     temp.reverse()
-//     for(let z=0;z<k;z++){
-//         nums[z]=temp[z]
-//     }
+var rotate = function(nums, k) {
+    k = k % nums.length;  // To handle cases where k > nums.length
+    const temp=[];
+    for(let i=nums.length-1;i>=nums.length-k;i--){
+        temp.push(nums[i]);
+    }
 
-// };
+    for(let j=nums.length-1;j>=k;j--){
+        nums[j]=nums[j-k];
+    }
+    temp.reverse()
+    for(let z=0;z<k;z++){
+        nums[z]=temp[z]
+    }
 
-//more readable
+};
+
+//more readable, more space complexity
 
 var rotate = function(nums, k) {
     k=k% nums.length
