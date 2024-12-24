a2e8da90bd653c2c0287655dd1bcf09c40b6df8f
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2024-12-24 18:16:20 +0530

    Time: 1 ms (89.58%) | Memory: 50.7 MB (51.58%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,15 +5,15 @@
  */
 
  //naive solutions O(n^2)
-var twoSum = (nums, target) => {
-    for(let i=0;i<nums.length;i++){
-        for(let j=1;j<nums.length;j++){
-            if(nums[i]+nums[j]===target){
-                return [i,j]
-            }
-        }
-    }
-};
+// var twoSum = (nums, target) => {
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=1;j<nums.length;j++){
+//             if(nums[i]+nums[j]===target){
+//                 return [i,j]
+//             }
+//         }
+//     }
+// };
 
 
 //optimised solution linear time O(n)
