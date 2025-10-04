8693ba4a1da65017fcf3408487a06d8f87cb81ef
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-10-04 16:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (55.46%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -23,31 +23,30 @@ public:
             i++;
         }
         long long res = 0;
-          while (i < s.length() && isdigit(s[i])) {
-              // Check for overflow before updating res
-              if (res > (LLONG_MAX - (s[i] - '0')) / 10) {
-                  if (symbol == '-') {
-                      return INT_MIN;
-                  } else {
-                      return INT_MAX;
-                  }
-              }
-              // so read and cout character by character
-              res = (long long)(res * 10) + (long long)(s[i] - '0');
-              i++;
-          }
+        while (i < s.length() && isdigit(s[i])) {
+            // Check for overflow before updating res
+            if (res > (LLONG_MAX - (s[i] - '0')) / 10) {
+                if (symbol == '-') {
+                    return INT_MIN;
+                } else {
+                    return INT_MAX;
+                }
+            }
+            // so read and cout character by character
+            res = (long long)(res * 10) + (long long)(s[i] - '0');
+            i++;
+        }
 
-          if (symbol == '-') {
-              if (res > (long long)INT_MAX + 1) {
-                  return INT_MIN;
-              }
-              return (int)-res;
-          } else {
-              if (res > INT_MAX) {
-                  return INT_MAX;
-              }
-              return (int)res;
-          }
-      }
-
+        if (symbol == '-') {
+            if (res > (long long)INT_MAX + 1) {
+                return INT_MIN;
+            }
+            return (int)-res;
+        } else {
+            if (res > INT_MAX) {
+                return INT_MAX;
+            }
+            return (int)res;
+        }
+    }
 };
