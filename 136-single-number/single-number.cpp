a5759200dc2fb9b1b718d09adb3ff446e1f0ca42
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-10-06 01:05:05 +0530

    Time: 12 ms (13.00%) | Memory: 24 MB (5.62%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //hashmap banao enjoy kro jiski freq 1 h uska maja - 
+        unordered_map<int,int> hashmap;
+        
+        for(int i=0;i<nums.size();i++){
+            if(hashmap.find(nums[i])==hashmap.end()){
+                hashmap.insert({nums[i],1});
+            }
+            else{
+                //already hai
+                hashmap[nums[i]]+=1;
+            }
+        }
+
+        for(auto &[num,freq] : hashmap){
+            if(freq==1){
+                return num;
+            }
+        }
+        return 0;
+    }   
+};
