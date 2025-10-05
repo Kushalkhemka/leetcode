4ffe7c505d783c80a0dce4e3b86d8b6aeda25727
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-10-06 01:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (83.30%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        //hashmap banao enjoy kro jiski freq 1 h uska maja - 
-        unordered_map<int,int> hashmap;
+    // int singleNumber(vector<int>& nums) {
+    //     //hashmap banao enjoy kro jiski freq 1 h uska maja - 
+    //     unordered_map<int,int> hashmap;
         
-        for(int i=0;i<nums.size();i++){
-            if(hashmap.find(nums[i])==hashmap.end()){
-                hashmap.insert({nums[i],1});
-            }
-            else{
-                //already hai
-                hashmap[nums[i]]+=1;
-            }
-        }
+    //     for(int i=0;i<nums.size();i++){
+    //         if(hashmap.find(nums[i])==hashmap.end()){
+    //             hashmap.insert({nums[i],1});
+    //         }
+    //         else{
+    //             //already hai
+    //             hashmap[nums[i]]+=1;
+    //         }
+    //     }
 
-        for(auto &[num,freq] : hashmap){
-            if(freq==1){
-                return num;
-            }
+    //     for(auto &[num,freq] : hashmap){
+    //         if(freq==1){
+    //             return num;
+    //         }
+    //     }
+    //     return 0;
+    // }   
+
+    int singleNumber(vector<int>& nums) {
+        int x = 0;
+        for (int num : nums) {
+            x ^= num; // All duplicates cancel; only the unique remains
         }
-        return 0;
+        return x; 
     }   
+
+    
 };
