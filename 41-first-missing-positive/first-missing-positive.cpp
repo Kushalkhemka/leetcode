17b501e20917ef970ccc0c01bd1b3c0f0e388122
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-10-12 23:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (50.70%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -5,9 +5,8 @@ public:
         int n = nums.size();
         int i = 0;
         while (i < n) {
-            // CHANGED: The last part of the condition is now more robust.
-            // It checks if the number at the current spot is the same as the
-            // number at its target destination. If they are the same, we don't swap.
+            //instead of nums[i]!=(i+1) use the nums[i] != nums[nums[i] - 1] condition to also handle the duplicate if the duplicate element is already at place then we don't need to swap again and thus avoid tle error preventing the infinite loop
+            //use cyclic sort and then just traverse from 0 to n-1 and check if element is  at correct index if not then return the index value (so we are returning the first such occurence) (this is done after sorting to ensure we are returning the missing number);
             if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                 swap(nums[nums[i] - 1], nums[i]);
             } else {
@@ -15,7 +14,6 @@ public:
             }
         }
 
-        // This second part of your code is perfectly correct.
         for (int j = 0; j < n; j++) {
             if (nums[j] != j + 1) {
                 return j + 1;
