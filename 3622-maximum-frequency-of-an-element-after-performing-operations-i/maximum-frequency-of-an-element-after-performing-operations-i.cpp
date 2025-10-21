192680fa6be508998879c828d634b907df309a7c
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-10-22 01:30:42 +0530

    Time: 12 ms (93.90%) | Memory: 100.8 MB (76.10%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    static int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        static constexpr int N=1e5+2;
-        int freq[N]={0}, sweep[N]={0}, mm=N, MM=0;
-        for(int x: nums){
-            freq[x]++;
-            const int x0=max(1, x-k), xN=min(x+k+1, N-1);
-            sweep[x0]++;
-            sweep[xN]--;
-            mm=min(mm, x0);
-            MM=max(MM, xN);
-        }
-        int ans=0, cnt=0;
-        for (int x=mm; x<=MM; x++){
-            cnt+=sweep[x];
-            ans=max(ans, freq[x]+min(cnt-freq[x], numOperations));
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int mx = *max_element(nums.begin(), nums.end());
+        int n = mx + k + 2;
+        vector<int> f(n, 0);
+        for (int x : nums) f[x]++;
+
+        vector<int> pre(n, 0);
+        pre[0] = f[0];
+        for (int i = 1; i < n; i++) pre[i] = pre[i - 1] + f[i];
+
+        int ans = 0;
+        for (int t = 0; t < n; t++) {
+            if (f[t] == 0 && numOperations == 0) continue;
+            int l = max(0, t - k), r = min(n - 1, t + k);
+            int tot = pre[r] - (l > 0 ? pre[l - 1] : 0);
+            int adj = tot - f[t];
+            int val = f[t] + min(numOperations, adj);
+            ans = max(ans, val);
         }
         return ans;
     }
