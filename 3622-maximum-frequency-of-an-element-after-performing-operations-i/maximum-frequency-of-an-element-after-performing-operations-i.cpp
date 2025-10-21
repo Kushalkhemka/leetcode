d02a4663b28f596d208d09391a81cae2026f190d
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-10-22 01:03:41 +0530

    Time: 215 ms (48.79%) | Memory: 122.3 MB (56.55%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,75 +1,62 @@
 class Solution {
 public:
-    int lowerBound(vector<int>&nums, int target){ //greater than just greater than target only in this case
-        int lo=0; int hi=nums.size()-1;
-        int ans=nums.size();
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(nums[mid]>=target){
-                ans=mid;
-                //go left
-                hi=mid-1;
-            }
-            else lo=mid+1;
-        }
-        return ans;
-    }
-
-
-
-     int upperBound(vector<int> &nums, int target){
-        int ans=nums.size();
-        int lo=0; int hi=nums.size()-1; 
-        while(lo<=hi){
-            int mid= lo+(hi-lo)/2;
-            if(nums[mid]<=target){
-                //store the current as the potentional ans
-                //search right half for possibilities of greater number which is less than or equal to x
-                lo=mid+1;
-            }
-            else{
-                //if mid is less than the ex and then search on the right half
-                ans=mid;
-                hi=mid-1;
-            }
-        }
-        return ans;
-    }
-
-    
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-    if (nums.empty()) return 0;
-    sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
-    // Step 1: Create a list of all important candidate targets.
-    // Using a set to handle duplicates automatically.
-    set<long long> candidates;
-    for (int num : nums) {
-        candidates.insert(num);
-        candidates.insert((long long)num - k);
-        candidates.insert((long long)num + k);
+        int ans = 0;
+        unordered_map<int, int> numCount;
+
+        int lastNumIndex = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != nums[lastNumIndex]) {
+                numCount[nums[lastNumIndex]] = i - lastNumIndex;
+                ans = max(ans, i - lastNumIndex);
+                lastNumIndex = i;
+            }
+        }
+
+        numCount[nums[lastNumIndex]] = nums.size() - lastNumIndex;
+        ans = max(ans, (int)nums.size() - lastNumIndex);
+
+        auto leftBound = [&](int value) {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] < value) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        };
+
+        auto rightBound = [&](int value) {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int mid = (left + right + 1) / 2;
+                if (nums[mid] > value) {
+                    right = mid - 1;
+                } else {
+                    left = mid;
+                }
+            }
+            return left;
+        };
+
+        for (int i = nums.front(); i <= nums.back(); i++) {
+            int l = leftBound(i - k);
+            int r = rightBound(i + k);
+
+            int tempAns;
+            if (numCount.count(i)) {
+                tempAns = min(r - l + 1, numCount[i] + numOperations);
+            } else {
+                tempAns = min(r - l + 1, numOperations);
+            }
+            ans = max(ans, tempAns);
+        }
+
+        return ans;
     }
-
-    int maxFinalFreq = 1;
-
-    // Step 2: Loop through each candidate target.
-    for (long long target : candidates) {
-        
-        // Step 3: Use your helper functions (which are correct!) to find the answer for this target.
-        
-        // Count elements already equal to target (important if target is in nums)
-        int initialFreq = upperBound(nums, target) - lowerBound(nums, target);
-
-        // Count all elements that could possibly become the target
-        // Note: The range must be cast to int for your helpers, or helpers must accept long long
-        int totalConvertible = upperBound(nums, target + k) - lowerBound(nums, target - k);
-        
-        int opsNeeded = totalConvertible - initialFreq;
-        
-        int successfulConversions = min(opsNeeded, numOperations);
-        
-        maxFinalFreq = max(maxFinalFreq, initialFreq + successfulConversions); 
-    }
-    return maxFinalFreq;
-}
 };
