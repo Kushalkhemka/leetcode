5bc18079841e46ab94297c7226bd48ecbf54183c
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-01 23:08:05 +0530

    Time: 91 ms (82.16%) | Memory: 263.1 MB (86.67%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,53 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        if (head == nullptr)
+        // Create an unordered_set for efficient lookup of values in nums
+        unordered_set<int> valuesToRemove(nums.begin(), nums.end());
+
+        // Handle the case where the head node needs to be removed
+        while (head != nullptr && valuesToRemove.count(head->val) > 0) {
+            ListNode* temp = head;
+            head = head->next;
+        //    delete temp;
+        }
+
+        // If the list is empty after removing head nodes, return nullptr
+        if (head == nullptr) {
             return nullptr;
-
-        // make a hashmap of nums value
-        unordered_set<int> hashmap;
-         for (int i = 0; i < nums.size(); i++) {
-            hashmap.insert(nums[i]);
         }
 
-        //get the head first which needs to be return
-        
-        // iterate LL
-        ListNode* HEAD = nullptr;
-        ListNode* curr = head;
-        ListNode* prev = nullptr;
-        int flag=0;
-        while (curr->next!= nullptr) {
-            if (hashmap.find(curr->val) != hashmap.end()) {
-                // mean exists so remove it
-                if(HEAD==nullptr){
-                    prev=curr;
-                    curr=curr->next;
-                }
-                else{
-                    prev->next=curr->next;
-                    curr=curr->next;
-                }
-            }
-            else{
-                if(HEAD==nullptr) HEAD=curr;
-                prev=curr; curr=curr->next; 
+        // Iterate through the list, removing nodes with values in the set
+        ListNode* current = head;
+        while (current->next != nullptr) {
+            if (valuesToRemove.contains(current->next->val)) {
+                // Store the node to be deleted
+                ListNode* temp = current->next;
+                // Skip the next node by updating the pointer
+                current->next = current->next->next;
+                // Delete the removed node
+            //    delete temp;
+            } else {
+                // Move to the next node
+                current = current->next;
             }
         }
-        //if need to remove last node then do
-        if (hashmap.find(curr->val) != hashmap.end()) {
-            prev->next=nullptr;
-        }
-        return HEAD?HEAD:curr;
+
+        return head;
     }
 };
