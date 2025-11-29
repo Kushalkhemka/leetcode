c30e0bd28dea6f132b4f771bc54291394651f1ca
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-29 22:54:19 +0530

    Time: 48 ms (83.95%) | Memory: 9.3 MB (95.24%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string &text1, string &text2) {
+        // dp lagao yeeeee
+        int n1 = text1.length(); int n2=n1;
+
+        // vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        vector<int> prev(n2 + 1, 0),
+            curr(n2 + 1, 0); // prev=dp[i-1] replace and curr=dp[i]
+        //------BASE CASE STARTS------//
+        // run the loop
+        for (int i = 0; i <= n2; i++) {
+            prev[i] = 0;
+        }
+        //------BASE CASE ENDS------//
+
+        //------MAKE LOOPS FOR CHANGING PARAMS IDX1 IDX2 STEP2------//
+        for (int i = 1; i <= n1; i++) {     // i tied to n1
+            for (int j = 1; j <= n2; j++) { // j tied to n2
+                // f(idx1, IDX2) -> f(i,j)
+                //------COPY RECURRENCE STEP3------//
+                if (text1[i - 1] == text2[j - 1]) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = 0 + max(prev[j], curr[j - 1]);
+                }
+                //------COPY RECURRENCE STEP3------//
+            }
+            prev = curr;
+        }
+        //------LOOP ENDS------//
+        return prev[n2];
+    }
+    int longestPalindromeSubseq(string s) {
+        // ye toh khud kr lungha smj gya index ke saath khelna h ek ka last and
+        // ek ka starting or make the copies of s as text1 and text2 and apply
+        // same lcs one
+        string text1 = s;
+        reverse(s.begin(), s.end());
+        string text2 = s;
+        return longestCommonSubsequence(text1,text2);
+    }
+};
