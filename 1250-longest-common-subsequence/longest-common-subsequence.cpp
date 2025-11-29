fbad610edacbc0bbf6d207130816073b7ffa2d10
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-29 21:27:17 +0530

    Time: 27 ms (55.55%) | Memory: 27.5 MB (28.51%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int fn(int idx1,int idx2, string &text1,string &text2,vector<vector<int>> &dp){
-        if(idx1<0 || idx2<0){
-            return 0;
-        }
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        if(text1[idx1]==text2[idx2]){
-            return dp[idx1][idx2]=1+fn(idx1-1,idx2-1,text1,text2,dp);
-        }
-        else{
-            return dp[idx1][idx2]=0+max(fn(idx1-1,idx2,text1,text2,dp),fn(idx1,idx2-1,text1,text2,dp));
-        }
-    }
     int longestCommonSubsequence(string text1, string text2) {
-        //dp lagao yeeeee
-        int n1=text1.length();
-        int n2=text2.length();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return fn(text1.length()-1,text2.length()-1,text1,text2,dp);
+        // dp lagao yeeeee
+        int n1 = text1.length();
+        int n2 = text2.length();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        //------BASE CASE STARTS------//
+        // run the loop
+        for (int i = 0; i <= n1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n2; i++) {
+            dp[0][i] = 0;
+        }
+        //------BASE CASE ENDS------//
+
+        //------MAKE LOOPS FOR CHANGING PARAMS IDX1 IDX2 STEP2------//
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                // f(idx1, IDX2) -> f(i,j)
+                //------COPY RECURRENCE STEP3------//
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                //------COPY RECURRENCE STEP3------//
+            }
+        }
+        //------LOOP ENDS------//
+        return dp[n1][n2];
     }
 };
