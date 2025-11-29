9f2cd1ef30168cb9934e952c851e1c0499ee44fc
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-29 21:41:19 +0530

    Time: 11 ms (97.70%) | Memory: 9 MB (99.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
+        // dp lagao yeeeee
         int n1 = text1.length();
         int n2 = text2.length();
-
-        vector<int> dp(n2 + 1, 0);    // changed: use single array
-
-        for (int i = 1; i <= n1; i++) {
-            int diag = 0;            // new: dp[i-1][j-1]
-
-            for (int j = 1; j <= n2; j++) {
-
-                int temp = dp[j];    // new: store dp[i-1][j] before overwriting
-
-                if (text1[i-1] == text2[j-1]) {
-                    dp[j] = 1 + diag;       // changed: use diag instead of prev[j-1]
-                } else {
-                    dp[j] = max(dp[j], dp[j-1]);
-                }
-
-                diag = temp;         // new: shift diagonal for next j
-            }
+        // vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        vector<int> prev(n2+1,0),curr(n2+1,0); //prev=dp[i-1] replace and curr=dp[i]
+        //------BASE CASE STARTS------//
+        // run the loop
+        for (int i = 0; i <= n2; i++) {
+           prev[i] = 0;
         }
+        //------BASE CASE ENDS------//
 
-        return dp[n2];
+        //------MAKE LOOPS FOR CHANGING PARAMS IDX1 IDX2 STEP2------//
+        for (int i = 1; i <= n1; i++) { //i tied to n1
+            for (int j = 1; j <= n2; j++) { //j tied to n2
+                // f(idx1, IDX2) -> f(i,j)
+                //------COPY RECURRENCE STEP3------//
+                if (text1[i - 1] == text2[j - 1]) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = 0 + max(prev[j], curr[j - 1]);
+                }
+                //------COPY RECURRENCE STEP3------//
+            }
+            prev = curr;
+
+        }
+        //------LOOP ENDS------//
+        return prev[n2];
     }
 };
