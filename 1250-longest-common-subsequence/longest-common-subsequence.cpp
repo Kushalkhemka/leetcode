ddf029774ce1685d23938b5637621f7e3a84d459
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-29 21:39:23 +0530

    Time: 15 ms (91.20%) | Memory: 9.3 MB (89.40%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,31 +4,31 @@ public:
         // dp lagao yeeeee
         int n1 = text1.length();
         int n2 = text2.length();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        // vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        vector<int> prev(n2+1,0),curr(n2+1,0); //prev=dp[i-1] replace and curr=dp[i]
         //------BASE CASE STARTS------//
         // run the loop
-        for (int i = 0; i <= n1; i++) {
-            dp[i][0] = 0;
-        }
         for (int i = 0; i <= n2; i++) {
-            dp[0][i] = 0;
+           prev[i] = 0;
         }
         //------BASE CASE ENDS------//
 
         //------MAKE LOOPS FOR CHANGING PARAMS IDX1 IDX2 STEP2------//
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+        for (int i = 1; i <= n1; i++) { //i tied to n1
+            for (int j = 1; j <= n2; j++) { //j tied to n2
                 // f(idx1, IDX2) -> f(i,j)
                 //------COPY RECURRENCE STEP3------//
                 if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 } else {
-                    dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = 0 + max(prev[j], curr[j - 1]);
                 }
                 //------COPY RECURRENCE STEP3------//
             }
+            prev = curr;
+
         }
         //------LOOP ENDS------//
-        return dp[n1][n2];
+        return prev[n2];
     }
 };
