064b1c5884ba255dbc3e48c38191919bb683421e
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-30 19:14:54 +0530

    Time: 3 ms (63.52%) | Memory: 10.2 MB (74.22%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -23,18 +23,26 @@ public:
         return true;
         
     }
-    void solve(int col, vector<string>&board, vector<vector<string>>&ans,int n){
+    void solve(int col, vector<string>&board, vector<vector<string>>&ans,int n,vector<int>&leftrow,vector<int>&upperDiagonal,vector<int>&lowerDiagonal){
         if(col==n){
             //base case reached enjoy terminate i mean
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftrow[row]==0 && upperDiagonal[n-1+col-row]==0 &&
+            lowerDiagonal[row+col]==0){
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
-                //pop out for the backtrack
+                leftrow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row]=1;
+                //next call
+                solve(col+1,board,ans,n,leftrow,upperDiagonal,lowerDiagonal);
+                //backtracking removal after solving the above further call it will return back and below is the return back code to let the other iterations could run up
                 board[row][col]='.';
+                leftrow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row]=0;
             }
         }
     }
@@ -45,8 +53,9 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
+        vector<int> leftrow(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
         //solve(col,board,ans,n(opt))
-        solve(0,board,ans,n);
+        solve(0,board,ans,n,leftrow,upperDiagonal,lowerDiagonal);
         return ans;
     }
 };
