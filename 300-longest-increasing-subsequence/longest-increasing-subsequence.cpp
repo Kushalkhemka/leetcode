80d86e09195595f581877ed57315a633f4aa269d
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-30 12:05:42 +0530

    Time: 280 ms (30.60%) | Memory: 296.5 MB (11.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int fn(int idx,int prev, vector<int>& nums,vector<vector<int>> &dp){
-        int n=nums.size();
-        if(idx==n) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        int not_take=0+fn(idx+1,prev,nums,dp);
-        int take=INT_MIN;
-        if(prev==-1 || nums[prev]<nums[idx]){
-            take=1+fn(idx+1,idx,nums,dp);
-        }
-        return dp[idx][prev+1]=max(take,not_take);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return fn(0,-1,nums,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+       
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prev = idx - 1; prev >= -1; prev--) {       
+
+                int not_take = dp[idx + 1][prev + 1];            
+
+                int take = INT_MIN;
+                if (prev == -1 || nums[prev] < nums[idx]) {
+                    take = 1 + dp[idx + 1][idx + 1];             
+                }
+
+                dp[idx][prev + 1] = max(take, not_take);        
+            }
+        }
+
+        return dp[0][0];    
     }
 };
