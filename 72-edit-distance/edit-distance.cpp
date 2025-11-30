cedda1148aa4cfa7a5b65f051059c7d0a8365489
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-11-30 20:57:59 +0530

    Time: 3 ms (91.79%) | Memory: 10.4 MB (96.18%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,30 +4,31 @@ public:
 
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        vector<int> prev(m+1,0),curr(m+1,0);
         //----BASE CASE---//
         for (int j = 0; j <= m; j++) {
-            dp[0][j] = j;
-        }
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = i;
+            prev[j] = j;
         }
         //----BASE CASE---//
 
         for (int i = 1; i <= n; i++) {
+            curr[0]=i;
             for (int j = 1; j <= m; j++) {
                 // match
+                
                 if (word1[i-1] == word2[j-1])
-                    dp[i][j] = 0 + dp[i - 1][j - 1];
+                    curr[j] = 0 + prev[j - 1];
                 // not match
                 // fn(i,j-1) -> insert
                 // fn(i-1,j) -> delete
                 // fn(i-1,j-1) -> replace
-                else dp[i][j] =
-                    1 + min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1]));
+                else curr[j] =
+                    1 + min(curr[j - 1], min(prev[j],prev[j - 1]));
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
