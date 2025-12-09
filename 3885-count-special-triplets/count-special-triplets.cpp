b2ac6fa6fca210059e6307c0887735fdb7656022
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2025-12-09 17:28:31 +0530

    Time: 561 ms (75.55%) | Memory: 263.8 MB (97.45%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,36 +1,59 @@
-#include <unordered_map>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        const int MOD = 1e9+7;
+        const int MOD = 1'000'000'007;
 
-        unordered_map<long long, long long> rightCount;
-        unordered_map<long long, long long> leftCount;
-
-        long long ans = 0;
         int n = nums.size();
 
-        for (int x : nums) {
-            rightCount[x]++;
+        // Step 1: Collect all values we will ever need:
+        // We need nums[j] and 2 * nums[j]
+        vector<long long> vals;
+        vals.reserve(2 * n);
+
+        for (long long x : nums) {
+            vals.push_back(x);
+            vals.push_back(2LL * x);
         }
 
+        // Step 2: Coordinate compression
+        sort(vals.begin(), vals.end());
+        vals.erase(unique(vals.begin(), vals.end()), vals.end());
+
+        auto getIndex = [&](long long v) {
+            return int(lower_bound(vals.begin(), vals.end(), v) - vals.begin());
+        };
+
+        int K = vals.size();
+
+        // Step 3: Replace unordered_map with fixed-size vectors
+        vector<long long> leftCount(K, 0), rightCount(K, 0);
+
+        for (long long x : nums) {
+            rightCount[getIndex(x)]++;
+        }
+
+        long long ans = 0;
+
+        // Step 4: Sweep j
         for (int j = 0; j < n; ++j) {
             long long x = nums[j];
-            rightCount[x]--;
+            int idxX = getIndex(x);
+            rightCount[idxX]--;
 
             long long target = 2LL * x;
+            int idxT = getIndex(target);
 
-            long long left  = leftCount[target];
-            long long right = rightCount[target];
+            long long left = leftCount[idxT];
+            long long right = rightCount[idxT];
 
             ans = (ans + (left * right) % MOD) % MOD;
 
-            leftCount[x]++;
+            leftCount[idxX]++;
         }
 
-        return static_cast<int>(ans % MOD);
+        return int(ans);
     }
 };
