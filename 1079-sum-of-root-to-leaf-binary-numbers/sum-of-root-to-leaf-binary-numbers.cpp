c8584972f0bc2b65c5f84d4caef788b83569038d
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-02-25 13:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (53.04%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -12,28 +12,35 @@
  */
 class Solution {
 public:
+    // int sum = 0;
+    // string temp;
+    // int stoi(string s) {
+    //     int x = 0;
+    //     for (char c : s) {
+    //         x = (x << 1) | (c - '0');
+    //     }
+    //     return x;
+    // }
+
     int sum = 0;
-    string temp;
-    int stoi(string s) {
-        int x = 0;
-        for (char c : s) {
-            x = (x << 1) | (c - '0');
-        }
-        return x;
-    }
-    void fn(TreeNode* root) {
-        if (root == nullptr)
+
+    void dfs(TreeNode* node, int cur) {
+        if (!node)
             return;
-        temp.push_back(root->val+'0');
-        if (root->left == nullptr && root->right == nullptr) {
-            sum += stoi(temp);
-            temp.pop_back();
+
+        cur = (cur << 1) | node->val;
+
+        if (!node->left && !node->right) {
+            sum += cur;
             return;
         }
-        
-        fn(root->left);
-        fn(root->right);
-        temp.pop_back();
+
+        dfs(node->left, cur);
+        dfs(node->right, cur);
     }
-    int sumRootToLeaf(TreeNode* root) { fn(root); return sum; }
+
+    int sumRootToLeaf(TreeNode* root) {
+        dfs(root, 0);
+        return sum;
+    }
 };
