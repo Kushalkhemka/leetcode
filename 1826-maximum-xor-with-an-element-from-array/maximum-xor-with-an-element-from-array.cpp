3328abc8d9afe1ef00c2aa122668363dbf0e8b3c
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-05-28 22:33:07 +0530

    Time: 1530 ms (5.00%) | Memory: 526.4 MB (5.00%) - LeetSync

---
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
@@ -0,0 +1,103 @@
+
+class Node {
+   public:
+    Node* links[2];
+    bool flg;
+
+    Node() {
+        for (int i = 0; i < 2; i++) {
+            links[i] = NULL;
+        }
+
+        flg = false;
+    }
+
+    bool containsKey(int b) {
+        return links[b] != nullptr;
+        // it exists if true
+    }
+
+    void put(int b, Node* node) {
+        // so we expect ch and pointer to node the ref node
+        links[b] = node;
+    }
+
+    // move to next node
+    Node* getNext(int b) { return links[b]; }
+
+    void setFlag() { flg = true; }
+
+    bool isEnd() { return flg; }
+};
+
+class Trie {
+   public:
+    Node* root;
+    Trie() { root = new Node(); }
+    void insert(int n) {
+        // first convert the number
+        Node* node = root;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (n >> i) & 1;
+            if (!node->containsKey(bit)) {
+                // not exists then we need to insert
+                node->put(bit, new Node());
+            }
+            node = node->getNext(bit);  // movve to next node
+        }
+
+        // after the loop finishes we will be on the last node make its flag
+        // true
+        node->setFlag();
+    }
+
+    int getMax(int n) {
+        Node* node = root;
+        int maxNum = 0;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (n >> i) & 1;  // like n>>31 & 1 gets us the MSB first so
+                                     // MSB -> LSB processing
+            int reqBit = bit ^ 1;  // which is the bit complement
+            if (node->containsKey(reqBit)) {
+                // reqbit exists
+                maxNum =
+                    maxNum |
+                    (1 << i);  // 1 because if reqbit^bit=1 always and we have
+                               // to push it i to left side so left shift
+                node = node->getNext(reqBit);
+            } else {
+                node = node->getNext(bit);
+            }
+        }
+        return maxNum;
+    }
+};
+
+
+class Solution {
+	public:	
+        static bool cmp(vector<int> a,vector<int>b){
+            return a[1] < b[1];
+        }
+    	vector<int> maximizeXor(vector<int>& nums, vector<vector<int> >& queries) {
+            Trie* T=new Trie();
+            vector<vector<int>> offline;
+            vector<int> ans(queries.size());
+            for(int i=0;i<queries.size();i++){
+                offline.push_back({queries[i][0], queries[i][1], i});
+            }
+            sort(offline.begin(), offline.end(), cmp);
+            sort(nums.begin(),nums.end());
+            int j=0;
+            for(int i=0; i<offline.size();i++){
+                //processing every query one by one
+                while(j < nums.size() & nums[j]<=offline[i][1]){
+                    T->insert(nums[j]);
+                    j++;
+                }
+                if(j==0) ans[offline[i][2]] =-1;
+                else ans[offline[i][2]]=(T->getMax(offline[i][0]));
+            }
+            return ans;
+    	}
+};
