693ae57ed00920e0bb3b90e9c327992b3626e215
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-08 23:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (75.59%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<string> ans;
-    void fn(string s, int n, int bc,
-            int bo) // bracket opening and brack closing (bo & bc)
-    {
+
+    void fn(string& s, int n, int bc, int bo) {
         if (bc == n && bo == n) {
             ans.push_back(s);
+            return;
         }
 
-        // add bo
         if (bo < n) {
-            fn(s + "(", n, bc, bo+1);
+            s.push_back('(');
+            fn(s, n, bc, bo + 1);
+            s.pop_back();
         }
 
-        if(bc < bo && bc < n){
-            fn(s + ")", n, bc+1, bo);
+        if (bc < bo) {
+            s.push_back(')');
+            fn(s, n, bc + 1, bo);
+            s.pop_back();
         }
     }
+
     vector<string> generateParenthesis(int n) {
-        fn("",n,0,0);
+        string s = "";
+        fn(s, n, 0, 0);
         return ans;
     }
 };
