b63c9017d6c76e6a5f8992e5a7c4e38746d1caf8
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-11 19:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (58.32%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,11 +1,11 @@
 class Solution {
    public:
-    void dfs(int newColor,int oldColor, int i, int j, vector<vector<int>> &vis,
+    void dfs(int newColor,int oldColor, int i, int j,
               vector<vector<int>> &grid) {
-        vis[i][j] = 1;
+    
         grid[i][j] = newColor;
 
-        // need to check in eight direction for adjacent 1 and then call dfs on
+        // need to check in four direction for adjacent 1 and then call dfs on
         // that i,j
         int dr[] = {-1, 0, 0, 1};
         int dc[] = {0, -1, 1, 0};
@@ -15,8 +15,8 @@ class Solution {
             int nc = j + dc[k];
 
             if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() &&
-                grid[nr][nc] == oldColor && !vis[nr][nc]) {
-                dfs(newColor,oldColor, nr, nc, vis, grid);
+                grid[nr][nc] == oldColor) {
+                dfs(newColor,oldColor, nr, nc, grid);
             }
         }
     }
@@ -24,9 +24,9 @@ class Solution {
                                   int newColor) {
         int n = image.size();     // no.of rows
         int m = image[0].size();  // no. of cols
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        dfs(newColor,image[sr][sc], sr, sc, vis, image);
+        int oldColor = image[sr][sc];
+        if (oldColor == newColor) return image;
+        dfs(newColor,oldColor, sr, sc, image);
 
         return image;
     }
