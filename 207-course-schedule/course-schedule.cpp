d820e9711927ea034edb867b28bd87b114a47bb3
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-13 15:49:12 +0530

    Time: 8 ms (34.25%) | Memory: 20.4 MB (14.19%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool ans = false;
+    void dfs(int node, const vector<vector<int>> &adj, vector<int> &visited,
+             vector<int> &pathVis) {
+        visited[node] = 1;
+        pathVis[node] = 1;
+        for (auto it : adj[node]) {
+            if (!visited[it]) {
+                dfs(it, adj, visited, pathVis);
+            } else {
+                if (pathVis[it]) {
+                    ans =true;
+                }
+            }
+        }
+        pathVis[node] = 0;
+    }
+
+    bool isCyclic(int N, const vector<vector<int>> &adj) {
+        vector<int> vis(N, 0);
+        vector<int> pathVis(N, 0);
+        for (int i = 0; i < N; i++) {
+            if (!vis[i]) {
+                dfs(i, adj, vis, pathVis);
+            }
+        }
+        return ans;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (auto it : prerequisites) {
+            adj[it[0]].push_back(it[1]); //graph is directed now
+        }
+        return !isCyclic(numCourses,adj);
+    }
+};
