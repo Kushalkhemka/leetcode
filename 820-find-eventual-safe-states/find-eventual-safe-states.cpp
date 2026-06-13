51847d6126b9f50227ebf2f22abf4196d22fcc7f
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-13 17:20:48 +0530

    Time: 3 ms (94.02%) | Memory: 51.7 MB (91.26%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,43 +1,41 @@
 class Solution {
    public:
-    void dfs(int node, const vector<vector<int>> &adj, vector<int>& visited,
-             vector<int>& ans) {
-        visited[node] = 1;  // current pathVis so visited[node]=1
-        for (const auto& it : adj[node]) {
-            if (!visited[it]) {
-                dfs(it, adj, visited, ans);
+    bool dfs(int node, const vector<vector<int>> &adj, vector<int>& visited) {
+        visited[node] = 1;  // current path
 
-                if (visited[it] == 3) {  // if child is unsafe, current node is also unsafe
-                    visited[node] = 3;   
+        for (auto it : adj[node]) {
+            if (visited[it] == 0) {
+                if (dfs(it, adj, visited)) {
+                    visited[node] = 3;  // changed: child unsafe, so current unsafe
+                    return true;
                 }
-
-            } else {
-                if (visited[it] == 1 || visited[it] == 3) {
-                    // cycle mil gyi to vis me jo bi 1 h unhe ban kr do
-                    // permanently for loop
-                    for (int i = 0; i < visited.size(); i++) {
-                        if (visited[i] == 1)
-                            visited[i] = 3;  // 3 means banned it can't be safe
-                                             // as one of the path of it's end
-                                             // in a non terminal state
-                    }
-                }
+            } else if (visited[it] == 1 || visited[it] == 3) {
+                visited[node] = 3;  // changed: cycle/unsafe neighbour found
+                return true;
             }
         }
-        if (visited[node] == 1)
-            visited[node] = 2;  // full processed only if it's not banned
-        if (visited[node] == 2) ans.push_back(node);
+
+        visited[node] = 2;  // safe
+        return false;
     }
 
     vector<int> eventualSafeNodes(const vector<vector<int>> &adj) {
+        int V=adj.size();
         vector<int> ans;
-        vector<int> visited(adj.size(), 0);
-        for (int i = 0; i < adj.size(); i++) {
-            if (!visited[i]) {
-                dfs(i, adj, visited, ans);
+        vector<int> visited(V, 0);
+
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == 0) {
+                dfs(i, adj, visited);
             }
         }
-        sort(ans.begin(), ans.end());
+
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == 2) {
+                ans.push_back(i);
+            }
+        }
+
         return ans;
     }
 };
