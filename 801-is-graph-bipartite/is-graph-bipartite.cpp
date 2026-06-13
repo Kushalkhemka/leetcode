0f55b371a5cb53e9013dd628eddb55171d8e222f
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-13 14:14:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (6.53%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,33 @@
+class Solution{
+public:
+    bool ans=true;
+    void dfs(int node, const vector<vector<int>> &adj,vector<int> &color) {
+    
+        
+        for (auto it : adj[node]) {
+            //here node is the parent so we need to assign opp color if not assigned or else check the color to be opposite of it other return false and take && with ans
+            if(color[it]==-1){
+                color[it]=!color[node];
+                dfs(it,adj,color);
+            }
+            else //it have some coloir assigned
+            {
+                if(color[it]==color[node]) ans=ans&&false; 
+            }
+        }
+    }
+
+    bool isBipartite( vector<vector<int>> graph)  {
+       
+        //color 0,1
+        vector<int> color(graph.size(),-1);
+
+        for (int i = 0; i < graph.size(); i++) {
+            if (color[i]==-1) {
+                dfs(i, graph, color);
+            }
+        }
+
+        return ans;
+    }
+};
