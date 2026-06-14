a3bc2ac166163322f87a5964f2c193a87a9cedc9
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-14 23:47:40 +0530

    Time: 4 ms (56.70%) | Memory: 124.5 MB (45.22%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,35 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        //easy shit nipta dungha haha
-        ListNode* temp=head;
-        int n=0;
-        while(temp!=nullptr){
-            n++;
-            temp=temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // find start of second half
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        vector<int>v(n/2);
-        int i=0;
-        temp=head;
-        while(i<n/2){
-            v[i]=temp->val;
-            temp=temp->next;i++;
+
+        // reverse second half
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+
+        while (curr != nullptr) {
+            ListNode* front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
         }
-        i=n/2-1;
-        while(i>=0){
-            v[i]+=temp->val;
-            temp=temp->next;i--;
+
+        // compare first half with reversed second half
+        ListNode* first = head;
+        ListNode* second = prev;
+        int ans = 0;
+
+        while (second != nullptr) {
+            ans = max(ans, first->val + second->val);
+            first = first->next;
+            second = second->next;
         }
-        return *max_element(v.begin(),v.end());
+
+        return ans;
     }
 };
