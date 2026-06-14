abcbd214ea00fce771e50cd6827a38337911bf5f
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-14 22:18:25 +0530

    Time: 12 ms (81.41%) | Memory: 23.4 MB (96.64%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,37 +2,36 @@
 class Solution {
    public:
     int shortestPathBinaryMatrix(vector<vector<int>> &grid) {
-        if(grid[0][0]!=0) return -1; //optimisation 
         int n = grid.size();
-        int m = grid[0].size();
-        if(n==1 && m==1) return 1;
-        queue<vector<int>> q;
-        int ans = 0;
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) return -1; //optimisation 
+        if(n==1) return 1;
+        queue<pair<int, int>> q;
+        int ans = 1;
         int dr[] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int dc[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-        q.push({0, 0, 0});
-        grid[0][0] = 2;
+        q.push({0, 0});
+        grid[0][0] = 1;
         while (!q.empty()) {
             int qs =
                 q.size();  // must be before for loop as size chnges in for loop
             for (int l = 0; l < qs; l++) {
-                int r = q.front()[0];
-                int c = q.front()[1];
-                int d = q.front()[2];
+                int r = q.front().first;
+                int c = q.front().second;
                 q.pop();
+                if (r == n - 1 && c == n - 1) return ans; // changed
 
                 for (int k = 0; k < 8; k++) {
                     int nr = r + dr[k];
                     int nc = c + dc[k];
 
-                    if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
                         grid[nr][nc] == 0) {
-                        if(nr==n-1 && nc==n-1) return d+2;
-                        grid[nr][nc] = 2;
-                        q.push({nr, nc,d+1});
+                        grid[nr][nc] = 1;
+                        q.push({nr, nc});
                     }
                 }
             }
+            ans++;
            
         }
         return -1;
