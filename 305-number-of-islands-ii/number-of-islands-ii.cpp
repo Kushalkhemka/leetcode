282b08465cc555fba25020283411cf53943f8dd1
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-15 21:13:11 +0530

    Time: 7 ms (77.11%) | Memory: 40.6 MB (91.03%) - LeetSync

--- 305-number-of-islands-ii/number-of-islands-ii.cpp
+++ 305-number-of-islands-ii/number-of-islands-ii.cpp
@@ -1,72 +1,81 @@
 class DisjointSet {
 public:
-    vector<int> rank;
+    vector<int> size;
     vector<int> parent;
+
     DisjointSet(int n) {
-        rank.resize(n, 0);
+        size.resize(n, 1);
         parent.resize(n);
+
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
     }
+
     int findUPar(int u) {
         if (parent[u] == u) {
             return u;
         }
+
         return parent[u] = findUPar(parent[u]);
     }
 
-    void unionByRank(int u, int v) {
+    void unionBySize(int u, int v) {
         int uPar_u = findUPar(u);
         int uPar_v = findUPar(v);
-        if (uPar_u == uPar_v)
+
+        if (uPar_u == uPar_v) {
             return;
-        if (rank[uPar_u] > rank[uPar_v]) {
-            // smaller attach to bigger rank one
-            parent[uPar_v] = uPar_u;
-        } else if (rank[uPar_u] < rank[uPar_v]) {
+        }
+
+        if (size[uPar_u] < size[uPar_v]) {
             parent[uPar_u] = uPar_v;
+            size[uPar_v] += size[uPar_u];
         } else {
-            parent[uPar_u] = uPar_v;
-            // upar_v attach to upar_u so rank of upar_u increass
-            rank[uPar_u]++;
+            parent[uPar_v] = uPar_u;
+            size[uPar_u] += size[uPar_v];
         }
     }
 };
+
 class Solution {
 public:
     vector<int> numIslands2(int m, int n, const vector<vector<int>>& positions) {
-        vector<vector<int>> grid(m, vector<int>(n, 0));
+        vector<char> grid(m * n, 0); // changed: 1D visited grid instead of 2D grid
         vector<int> ans;
+        ans.reserve(positions.size()); // changed: avoids repeated vector reallocations
+
         DisjointSet ds(m * n);
+
         int dr[] = {-1, 0, 0, 1};
         int dc[] = {0, -1, 1, 0};
+
         int cnt = 0;
 
-        for (auto x : positions) {
+        for (const auto& x : positions) { // changed: avoid copying each position vector
             int r = x[0];
             int c = x[1];
 
-            if (grid[r][c] == 1) { // to avoid duplicate grid[i][j] processing
+            int node = r * n + c; // changed: calculate node before duplicate check
+
+            if (grid[node] == 1) { // changed: duplicate check using 1D grid
                 ans.push_back(cnt);
                 continue;
             }
 
-            grid[r][c] = 1;
+            grid[node] = 1; // changed: mark land in 1D grid
             cnt++;
-
-            int node = r * n + c;
 
             for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
 
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
-                    grid[nr][nc] == 1) {
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
                     int adjNode = nr * n + nc;
 
-                    if (ds.findUPar(node) != ds.findUPar(adjNode)) {
-                        ds.unionByRank(node, adjNode);
+                    if (grid[adjNode] == 1 &&
+                        ds.findUPar(node) != ds.findUPar(adjNode)) {
+                        ds.unionBySize(node, adjNode);
                         cnt--;
                     }
                 }
@@ -74,6 +83,7 @@ public:
 
             ans.push_back(cnt);
         }
+
         return ans;
     }
 };
