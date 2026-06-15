f30363911995c2026d7fa535dad96fc87a201954
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-16 00:03:01 +0530

    Time: 11 ms (13.08%) | Memory: 28.4 MB (46.01%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    void dfs(int i, int j, vector<vector<int>>& vis,
+             const vector<vector<int>>& grid, int &area) {
+        vis[i][j] = 1;
+        area++;
+
+        // need to check in eight direction for adjacent 1 and then call dfs on
+        // that i,j
+        int dr[] = {-1, 0, 0, 1};
+        int dc[] = {0, -1, 1, 0};
+
+        for (int k = 0; k < 4; k++) {
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+
+            if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() &&
+                grid[nr][nc] == 1 && !vis[nr][nc]) {
+                dfs(nr, nc, vis, grid,area);
+            }
+        }
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();    // no.of rows
+        int m = grid[0].size(); // no. of cols
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int maxi=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == 1) {
+                    int area=0;
+                    dfs(i, j, vis, grid,area);
+                    maxi=max(area,maxi);
+                }
+            }
+        }
+        return maxi;
+    }
+};
