f33cc06f4849eae7e1ab15913593cb5e37963c6a
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-15 23:07:09 +0530

    Time: 9 ms (32.20%) | Memory: 105.7 MB (17.13%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
+    int ans = 0;
+
+    bool isValid(int nr, int nc, int m, int n) {
+        return nr >= 0 && nr < m && nc >= 0 && nc < n; 
+    }
+
+    void dfs(int i, int j, vector<vector<int>>& vis,
+             const vector<vector<int>>& grid, int m, int n) { 
+        vis[i][j] = 1;
+
+        // need to check in four directions only: up, left, right, down
+        int dr[] = {-1, 0, 0, 1};
+        int dc[] = {0, -1, 1, 0};
+
+        for (int k = 0; k < 4; k++) {
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+
+            if (!isValid(nr, nc, m, n) || grid[nr][nc] == 0) {
+                ans++;
+            } else if (!vis[nr][nc]) {
+                dfs(nr, nc, vis, grid, m, n); 
+            }
+        }
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        int ans = 0;
+        vector<vector<int>> visited(m, vector<int>(n, 0));
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    ans += 4;
-
-                    if (i > 0 && grid[i - 1][j] == 1) ans -= 2; // shared edge with upper land
-                    if (j > 0 && grid[i][j - 1] == 1) ans -= 2; // shared edge with left land
+                    dfs(i, j, visited, grid, m, n); 
+                    return ans;
                 }
             }
         }
 
-        return ans;
+        return 0;
     }
 };
