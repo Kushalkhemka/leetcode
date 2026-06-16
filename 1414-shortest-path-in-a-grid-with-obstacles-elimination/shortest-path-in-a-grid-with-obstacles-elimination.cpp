65588d1dcdd67d9dc11fc89cfb1c25e0b94fb792
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-16 20:50:27 +0530

    Time: 30 ms (65.19%) | Memory: 20.1 MB (65.19%) - LeetSync

---
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<vector<int>> q;
+        int ans = 0;
+        int dr[] = {-1, 0, 0, 1};
+        int dc[] = {0, -1, 1, 0};
+
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+
+        q.push({0,0,0,k});
+        vis[0][0] = k; 
+        while (!q.empty()) {
+            int qs =
+                q.size(); // must be before for loop as size chnges in for loop
+            for (int l = 0; l < qs; l++) {
+                int r = q.front()[0];
+                int c = q.front()[1];
+                int d = q.front()[2];
+                int obs=q.front()[3];
+                q.pop();
+
+                if (r == n-1 && c == m-1)
+                    return d;
+
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + dr[i]; 
+                    int nc = c + dc[i]; 
+                    bool flg=nr >= 0 && nr < n && nc >= 0 && nc < m;
+
+                    if (flg && grid[nr][nc] == 0) {
+                        if (obs <= vis[nr][nc]) continue; //skip only if this cell was reached with better/equal obs left
+
+                        if (nr == n-1 && nc == m-1)
+                            return d + 1;
+
+                        vis[nr][nc] = obs; 
+                        q.push({nr, nc, d + 1, obs});
+                    }
+                    else if(flg && grid[nr][nc] == 1 && obs > 0) {
+
+                        if (obs - 1 <= vis[nr][nc]) continue; 
+                        if (nr == n-1 && nc == m-1)
+                            return d + 1;
+
+                        vis[nr][nc] = obs - 1; 
+                        q.push({nr, nc, d + 1, obs - 1}); 
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
