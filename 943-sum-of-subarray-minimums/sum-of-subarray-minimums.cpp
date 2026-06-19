d90788ce307f8c9bc49248b5aafb2ef26c9a7d4c
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-19 12:10:20 +0530

    Time: 63 ms (27.17%) | Memory: 193.3 MB (24.53%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,32 +1,64 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        const int MOD = 1e9 + 7;
+    int sumSubarrayMins(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> s;
 
-        vector<long long> dp(n, 0);
-        vector<int> st;
+        vector<int> pse(n, -1);
+        vector<int> nse(n, n);
 
-        long long ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[i] < arr[st.back()]) {
-                st.pop_back();
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.empty()) {
+                s.push(i);
+                continue;
             }
 
-            if (st.empty()) {
-                dp[i] = 1LL * (i + 1) * arr[i];
+            while (!s.empty() && nums[s.top()] >= nums[i]) {
+                s.pop();
+            }
+
+            if (s.empty()) {
+                s.push(i);
             } else {
-                int j = st.back();
-                dp[i] = dp[j] + 1LL * (i - j) * arr[i];
+                nse[i] = s.top();
+                s.push(i);
             }
-
-            dp[i] %= MOD;
-            ans = (ans + dp[i]) % MOD;
-
-            st.push_back(i);
         }
 
-        return (int)ans;
+        while (!s.empty()) {
+            s.pop();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (s.empty()) {
+                s.push(i);
+                continue;
+            }
+
+            while (!s.empty() && nums[s.top()] > nums[i]) {
+                s.pop();
+            }
+
+            if (s.empty()) {
+                s.push(i);
+            } else {
+                pse[i] = s.top();
+                s.push(i);
+            }
+        }
+        // why n-> including the i so n-i gives the number of element ahead of
+        // it in whihc it is minimum that start from i so [3,1,2,4] , here for
+        // "2" n=4, so it got 2 subarrays [2,4] and the [2] for pse it is i-(-1)
+        // if no pse, then i+1 so it incldues the i, here [2] only one subarray,
+        // 1*2=2 subarray
+        long long sum = 0;
+        int MOD = 1e9 + 7;
+
+        for (int i = 0; i < n; i++) {
+            int a = (1LL * (i - pse[i]) * nums[i]) % MOD;
+            sum = (sum + (1LL * (nse[i] - i) * a) % MOD) % MOD;
+        }
+
+        return (int)sum;
     }
 };
