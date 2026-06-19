48deb957f564645d4b0d96a3945a753351fd004c
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-19 20:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (52.32%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -4,9 +4,7 @@ public:
         if(i==0 && j==0) return 1;//return 1 to count this valid path which reached this cell
         if(i>m-1 || j>n-1 || i<0 || j<0) return 0; //handle out of bound indexes
         if(dp[i][j]!=-1) return dp[i][j];
-        int up=fn(i-1,j,m,n,dp);
-        int left=fn(i,j-1,m,n,dp);
-        return dp[i][j]=up+left;
+        return dp[i][j]=fn(i-1,j,m,n,dp)+fn(i,j-1,m,n,dp);
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
