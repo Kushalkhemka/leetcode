84266bcb51fb84480b0539b79770dd2398edea4b
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-20 12:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (25.79%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool checkValidString(string st) {
+        stack<int> s1;
+        stack<int> s2;
+        int cnt = 0;
+        for (int i = 0; i < st.size(); i++) {
+            if (st[i] == '(') {
+                s1.push(i);
+            } else if (st[i] == '*') {
+                s2.push(i);
+            } else {
+                // c is )
+                if (s1.empty() && s2.empty())
+                    return false;
+                else if (!s1.empty())
+                    s1.pop();
+                else {
+                    // treated as opening bracket
+                    if (s2.top() < i)
+                        s2.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+
+        while (!s2.empty() && !s1.empty() && s2.top() > s1.top()) {
+            s1.pop();
+            s2.pop();
+        }
+
+        if (s1.empty())
+            return true;
+        return false;
+    }
+};
