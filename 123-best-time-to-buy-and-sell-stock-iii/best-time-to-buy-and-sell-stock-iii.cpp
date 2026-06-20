715ec32464cb63a148cd4533608d26cd42451180
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-21 00:23:45 +0530

    Time: 453 ms (22.19%) | Memory: 224 MB (16.84%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,35 +1,32 @@
 
 
 class Solution {
-public:
-    int maxProfit(vector<int>& arr) {
-        int maxCap = 2;
-        int n=arr.size();
-        vector<vector<vector<int>>> dp(
-            n + 1, vector<vector<int>>(2, vector<int>(maxCap + 1, 0))
-        );
-
-        for (int idx = n - 1; idx >= 0; idx--) {
-            for (int flg = 0; flg <= 1; flg++) {
-                for (int cap = 1; cap <= maxCap; cap++) {
-
-                    int nothing = dp[idx + 1][flg][cap];
-                    int something = 0;
-
-                    if (flg == 0) {
-                        // buy
-                        something = -arr[idx] + dp[idx + 1][1][cap]; //  cap same because transaction not completed yet
-                    } 
-                    else {
-                        // sell
-                        something = arr[idx] + dp[idx + 1][0][cap - 1]; //  cap decreases after sell because transaction completes
-                    }
-
-                    dp[idx][flg][cap] = max(nothing, something);
-                }
-            }
+   public:
+    int fn(int idx, bool flg, int cnt, const vector<int>& arr, const int n,
+           vector<vector<vector<int>>>& dp) {
+        // 1->already bought 1 share holding -> either do nothing, or sell
+        // 0->either buy or skip idx
+        if (idx == n) {
+            return 0;
         }
-
-        return dp[0][0][2];
+        if(flg==0 && cnt==0) return 0;
+        if (dp[idx][flg][cnt] != -1) return dp[idx][flg][cnt];
+        int nothing = fn(idx + 1, flg, cnt, arr, n, dp);
+        int something = 0;
+        if (flg) {
+            // sell
+            something = arr[idx] + fn(idx + 1, 0, cnt, arr, n, dp);
+        } else {
+            // buy
+            if (cnt > 0)
+                something = -arr[idx] + fn(idx + 1, 1, cnt - 1, arr, n, dp);
+        }
+        return dp[idx][flg][cnt] = max(nothing, something);
+    }
+    int maxProfit(const vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+    
+        return fn(0, 0, 2, arr, n, dp);
     }
 };
