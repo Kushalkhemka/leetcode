21a9d96e95cc9ddcb1544df740ff57b517746c91
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-20 10:20:33 +0530

    Time: 34 ms (15.36%) | Memory: 80.2 MB (80.27%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& g, vector<int>& s) {
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int i=0;int j=0;
+        int ng=g.size();int ns=s.size();
+        while(i<ng && j<ns){
+            if(s[j]>=g[i]) {j++;i++;}
+            else j++;
+        }
+        return i;
+    }
+};
