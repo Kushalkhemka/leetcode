9e0e8b8224436c77d4478cfeb4e75a61b6b5f517
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-20 08:17:42 +0530

    Time: 1 ms (19.67%) | Memory: 12.7 MB (73.15%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int fn(int i, int j, int m, int n, vector<vector<int>>& dp,
            vector<vector<int>>& triangle) {
@@ -30,5 +30,22 @@ public:
         }
 
         return ans; 
+    }
+};
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+
+        vector<int> dp = triangle[m - 1];
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
     }
 };
