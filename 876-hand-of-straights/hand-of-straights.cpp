7dae984c7e19b89542b5704e609fb78960ed8c06
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-21 15:17:14 +0530

    Time: 41 ms (44.19%) | Memory: 32.4 MB (40.97%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        if (nums.size() % k != 0)
+            return false;
+
+        map<int, int> freqMap;
+
+        for (int x : nums) {
+            freqMap[x]++;
+        }
+
+        for (auto& [val, fVal] : freqMap) {
+            if (fVal > 0) {
+                int groups = fVal;
+
+                for (int i = val; i < val + k; i++) {
+                    auto it = freqMap.find(i);
+
+                    if (it != freqMap.end() && it->second >= groups) {
+                        it->second -= groups;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
