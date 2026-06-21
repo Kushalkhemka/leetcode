9048312b423b39479a65a6ac1ec85efb1b951e40
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-21 18:09:55 +0530

    Time: 17 ms (42.38%) | Memory: 22 MB (99.73%) - LeetSync

---
+++ 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDistance(string moves) {
+        int x=0;int y=0; int count=0;
+        for(auto &c:moves){
+            if(c=='L') x-=1;
+            else if(c=='D') y-=1;
+            else if(c=='U') y+=1;
+            else if(c=='R') x+=1;
+            else count++;
+        }
+        return abs(x)+abs(y)+count;
+    }
+};
