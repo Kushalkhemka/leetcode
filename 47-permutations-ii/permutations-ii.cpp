abc00849d7ae331c88d2693874d8b6505962ed0b
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-22 16:42:19 +0530

    Time: 3 ms (61.36%) | Memory: 12 MB (57.58%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    set<vector<int>> ans;
+    vector<vector<int>> ans;
+
     void fn(vector<int>& nums, int n, vector<int>& visited, vector<int>& curr) {
         if (curr.size() == n) {
-            ans.insert(curr);
+            ans.push_back(curr);
             return; // base case
         }
         // mark one as visited
         for (int i = 0; i < n; i++) {
             if (visited[i] == 0) {
+                if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0)
+                    continue;
                 curr.push_back(nums[i]);
-                visited[i]=1;
-                fn(nums, n,visited,curr);
-                visited[i]=0;
+                visited[i] = 1;
+                fn(nums, n, visited, curr);
+                visited[i] = 0;
                 curr.pop_back();
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> visited(n,0);
-        vector<vector<int>> ansArr;
-        vector<int>curr;
-        fn(nums,n,visited,curr);
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<int> visited(n, 0);
+        // vector<vector<int>> ansArr;
+        vector<int> curr;
+        fn(nums, n, visited, curr);
 
-        for(auto x:ans){
-            ansArr.push_back(x);
-        }
-        return ansArr;
+        // for(auto x:ans){
+        //     ansArr.push_back(x);
+        // }
+        return ans;
     }
-
 };
