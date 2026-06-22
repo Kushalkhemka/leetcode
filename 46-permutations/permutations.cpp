004d92ac082548911e009a828c2cdf8e5492fae0
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-22 13:39:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (27.44%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void fn(vector<int>& nums, int n, vector<int>& visited, vector<int>& curr) {
+        if (curr.size() == n) {
+            ans.push_back(curr);
+            return; // base case
+        }
+        // mark one as visited
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                curr.push_back(nums[i]);
+                visited[i]=1;
+                fn(nums, n,visited,curr);
+                visited[i]=0;
+                curr.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> visited(n,0);
+        vector<int>curr;
+        fn(nums,n,visited,curr);
+        return ans;
+    }
+};
