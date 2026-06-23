f45627ff0036d590b421a0c616deb6975a13444e
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-23 13:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (39.90%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<int, int> freqMap;
+        for (auto& c : text) {
+            freqMap[c]++;
+        }
+        vector<char> arr = {'b', 'a', 'l', 'l', 'o', 'o', 'n'};
+        int ans = INT_MAX;
+        if (freqMap['o'] > 0)
+            freqMap['o'] /= 2;
+        if (freqMap['l'] > 0)
+            freqMap['l'] /= 2;
+        for (int i = 0; i < arr.size(); i++) {
+            ans = min(ans, freqMap[arr[i]]);
+        }
+        return ans;
+    }
+};
