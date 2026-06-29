52432e77da8731051f93e5977aa03a3ab1ced035
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-06-29 19:48:14 +0530

    Time: 125 ms (29.20%) | Memory: 13.6 MB (66.41%) - LeetSync

---
+++ 277-find-the-celebrity/find-the-celebrity.cpp
@@ -0,0 +1,32 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        vector<int> indegree(n);
+  
+
+        vector<int> candidates;
+        ///first go for outdegree
+        for(int i=0;i<n;i++){
+            int flg=0;
+            for(int j=0;j<n;j++){
+                if(knows(i,j)&& i!=j) {flg=1;break;}
+            }
+            if(!flg) candidates.push_back(i);
+        }
+
+        for(auto&c:candidates){
+            int flg=0;
+            for(int i=0; i<n;i++){
+                if(!knows(i,c) && i!=c){
+                    flg=1; break;
+                }
+            }
+            if(!flg) return c;
+        }
+        return -1;
+
+    }
+};
