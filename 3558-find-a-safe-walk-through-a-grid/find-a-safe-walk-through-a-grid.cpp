93dfa8d4fe72fd37985ea6db6cdcf6911625b1e7
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-07-02 18:41:29 +0530

    Time: 25 ms (46.07%) | Memory: 37.3 MB (31.28%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,39 @@
+#define P pair<int,pair<int,int>>
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<P> q;
+        vector<vector<int>> best(n, vector<int>(m, -1));
+        int startHealth=health-grid[0][0];
+        if(startHealth<1) return false;
+        q.push({startHealth,{0,0}});
+        best[0][0] = startHealth;
+       
+        int dr[]={-1,0,1,0};
+        int dc[]={0,-1,0,1};
+        while(!q.empty()){
+            int r=q.front().second.first;
+            int c=q.front().second.second;
+            int h=q.front().first;
+
+            q.pop();
+            if(r==n-1 && c==m-1 && h>=1) return true; 
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+
+                if(nr<n && nc<m && nr>=0 && nc>=0){
+                    int newHealth=h-grid[nr][nc];
+
+                    if(newHealth>=1 && newHealth > best[nr][nc]){
+                        q.push({newHealth,{nr,nc}});
+                        best[nr][nc]=newHealth;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
