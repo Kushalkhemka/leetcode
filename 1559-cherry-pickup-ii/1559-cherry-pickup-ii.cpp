00ef481392e4f0cf52045ccadd1d0d059fc917bf
Kushal Khemka <72255307+Kushalkhemka@users.noreply.github.com>
2026-07-15 01:02:41 +0530

    1559-CHERRY-PICKUP-II Stats: Time: 59 ms (33.52%), Space: 13.5 MB (94.24%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -41,9 +41,46 @@ public:
     int cherryPickup(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(m, -1)));
+        vector<vector<int>> front(m, vector<int>(m, -1));
+        vector<vector<int>> curr(m, vector<int>(m, -1));
 
-        return fn(0, 0, m - 1, n, m, matrix, dp);
+        // base case
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    front[j1][j2] = matrix[n - 1][j1];
+                else
+                    front[j1][j2] = matrix[n - 1][j1] + matrix[n - 1][j2];
+            }
+        }
+        // n-1 plane is already filled so move to n-2 to 0
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    int maxi = INT_MIN;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            // for one dj1, dj2 have 3 possible moves
+                            int value = 0;
+                            if (j1 == j2)
+                                value = matrix[i][j1];
+                            // only one out of two will be executed
+                            else
+                                value = matrix[i][j2] + matrix[i][j1];
+                            if (j1 + dj1 >= 0 && j1 + dj1 < m &&
+                                j2 + dj2 >= 0 && j2 + dj2 < m) {
+                                value += front[j1 + dj1][j2 + dj2];
+                            } else
+                                value += -1e8;
+                            maxi = max(maxi, value);
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            front = curr;
+        }
+
+        return curr[0][m - 1];
     }
 };
